5122795ab4fe9b015a758ae4adf61b65739f32b3
Taghreed Louz <121046503+TaghreedLouz@users.noreply.github.com>
2023-07-01 23:50:04 +0300

    login update

--- app/src/main/java/com/example/client/Ui/Activities/Login/LoginPresenter.java
+++ app/src/main/java/com/example/client/Ui/Activities/Login/LoginPresenter.java
@@ -1,6 +1,37 @@
 package com.example.client.Ui.Activities.Login;
 
 
+import com.example.client.Model.CliantsNumbers;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 public class LoginPresenter {
 
+    private LoginView view;
+    private FirebaseFirestore firestore;
+
+    public LoginPresenter(LoginView view) {
+        this.view = view;
+        firestore = FirebaseFirestore.getInstance();
+    }
+
+    public void checkDriverIsExist(String mobile) {
+        firestore.collection("Benf_Numbers").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                boolean numberFound = false;
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    CliantsNumbers num = document.toObject(CliantsNumbers.class);
+                    if (mobile.equals(String.valueOf(num.getMobile()))) {
+                        numberFound = true;
+                        view.isDriver(num);
+                        break;
+                    }
+                }
+                if (!numberFound) {
+                    view.numberNotFound();
+                }
+            } else {
+                view.onFail(task.getException());
+            }
+        });
+    }
 }
